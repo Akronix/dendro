c8c42bb858e274439e6510afb5b621b8e561f957
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2024-02-06 11:06:59 +0100

    Processing log and updated code to process Miedes env data

--- process-env.R
+++ process-env.R
@@ -3,20 +3,22 @@ source("lib-dendro.R")
 library(readr) # for write_csv() function
 library(plyr) # for empty()
 library(tidyverse) # for %>%
+library(glue)
 
 library(myClim)
 
 ### DEFINE GLOBAL VARS ###
-PATH = '/home/akronix/workspace/dendro';
+PATH = dirname(rstudioapi::getActiveDocumentContext()$path);
 setwd(PATH)
 
 ts_start<-"2022-03-16 11:00:00" # # from March 16 (1 day after installation)
 ts_end<-"2023-09-27 08:00:00" # last timestamp of downloaded data
 
-ENVIRONMENT_DIR = 'raw/Corbalan-env'
-OUTPUT_ENV_DIR = 'processed/Corbalan-env-processed'
+PLACE = 'Miedes'
+ENVIRONMENT_DIR = glue('raw/{PLACE}-env')
+OUTPUT_ENV_DIR = glue('processed/{PLACE}-env-buffer-toclear')
 
-SOIL_TYPE = "sandy loam B"
+SOIL_TYPE = "sandy loam A"
 
 OUTPUT_PATH = file.path(PATH, OUTPUT_ENV_DIR)
 if (!dir.exists(OUTPUT_PATH)) {dir.create(OUTPUT_PATH)}
@@ -60,16 +62,3 @@ for (filename in list_files) {
 
 str(dfs.all)
 
-db.env <- subset(dfs.all, select = c("ts", "temp", "vwc") )
-
-# Do mean of all sensors
-db.env <- dfs.all %>%
-  #filter(ts < ymd_hms("2022-12-31 00:00:00")) %>%
-  group_by(ts) %>%
-  dplyr::summarise(temp = mean(temp, na.rm = TRUE), VWC = mean(vwc, na.rm = T))
-
-summary(db.env)
-
-# write aggregated data to file.
-if (!dir.exists(file.path(OUTPUT_PATH, 'aggregated'))) {dir.create(file.path(OUTPUT_PATH, 'aggregated'))}
-write_csv(db.env, file.path(OUTPUT_PATH, 'aggregated', "proc-agg-temp&vwc.csv"), append = F, col_names = T)
