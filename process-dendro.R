d976d8f813d22404d4a4c3d272fb5bc18a5a324b
Abel 'Akronix' Serrano Juste <akronix5@gmail.com>
2024-04-22 11:00:46 +0200

    Reprocessed 92222165 up to mar-24

--- process-dendro.R
+++ process-dendro.R
@@ -19,11 +19,12 @@ if (length(args) > 0 & !is.na(as.numeric(args[1])) ){
   SELECTED_DENDROMETER = as.character(args[1])
   SAVE <- T # to save output csv processed file at the end of the script
 } else {
-  SELECTED_DENDROMETER = "92222160"
+  SELECTED_DENDROMETER = "92222165"
 }
 
-TOL_OUT = 14
 TOL_JUMP = 12
+TOL_OUT = 14
+
 DATE_FORMAT = "%Y.%m.%d %H:%M" # Default
 
 # GENERAL GLOBAL VARIABLES #
@@ -111,7 +112,7 @@ dendro_raw_plot <-
   scale_x_datetime(date_breaks = "1 month", date_labels = "%m-%y") +
   theme_bw()
   
-dendro_raw_plot
+# dendro_raw_plot
 
 raw.output.fn <- file.path( OUTPUT_ASSETS_DIR, paste( db$series[1] ,"-",'raw data plot.png'))
 
@@ -191,14 +192,14 @@ final_processed_data <- dendro_data_L2;
 # DANGER! MANUAL CORRECTIONS #
 final_processed_data <- corr_dendro_L2(dendro_L1 = dendro_data_L1,
                                        dendro_L2 = dendro_data_L2,
-                                       reverse = c(1),
+                                       # reverse = c(1),
                                        # force.now = c("2023-10-09 23:00:00"),
                                        #               "2023-10-16 08:00:00"),
                                        # #               "2023-12-13 11:30:00"
                                                       # ),
-                                       force = c("2022-08-17 00:00:00"),
+                                       # force = c("2022-08-17 00:00:00"),
                                        # n_days = 1,
-                                       # delete = c("2022-08-24 19:00:00", "2022-08-24 19:00:00"),
+                                       delete = c("2022-05-21 16:30:00", "2022-11-12 04:00:00"),
                                                   # "2023-01-22 16:45:00", "2023-01-22 17:00:00",
                                                   # "2023-01-27 16:15:00", "2023-01-27 16:15:00",
                                                   # "2023-10-16 08:15:00", "2023-10-17 17:15:00"
